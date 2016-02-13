76230af6c788f8c3b68f9181938a7fe0d2927d5d
joshr <jreuben111@gmail.com>
2016-02-13 09:04:53 +0200

    some for comprehensions

--- src/test/scala/BasicsTest.scala
+++ src/test/scala/BasicsTest.scala
@@ -19,4 +19,18 @@ class BasicsTest extends FlatSpec with Matchers {
 
   }
 
+  "for comprehensions" should "be shorthand for map ops" in {
+    val xs = 1 to 5
+    val ys = 1 until 6 // same
+    val zs = 1 to 10 by 2
+    xs should be (ys)
+
+    // filter/map
+    (for (x <- xs if x%2 == 0) yield x*10)    should be (xs.filter(_%2 == 0).map(_*10))
+    // destructuring bind
+    (for ((x,y) <- xs zip ys)  yield x*y)     should be ((xs zip ys) map { case (x,y) => x*y })
+    // cross product
+    (for (x <- xs; y <- ys)    yield x*y)     should be (xs flatMap {x => ys map {y => x*y}})
+  }
+
 }
