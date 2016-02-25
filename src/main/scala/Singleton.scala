9cb450f7e206062beaaba78ef8e3377be9c122f5
joshr <jreuben111@gmail.com>
2016-02-25 06:53:32 +0200

    singleton

---
+++ src/main/scala/Singleton.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by joshr on 25/02/2016.
+  */
+class Singleton private {}
+
+object Singleton {
+  private val singleton = new Singleton
+  def apply = singleton
+}
--- src/test/scala/BasicsTest.scala
+++ src/test/scala/BasicsTest.scala
@@ -25,12 +25,21 @@ class BasicsTest extends FlatSpec with Matchers {
     val zs = 1 to 10 by 2
     xs should be (ys)
 
+    val fc = for {
+      i <- (0 until 2);
+      j <- (2 until 4)
+    } yield (i, j)
+    fc should be (Vector((0,2), (0,3), (1,2), (1,3)))
     // filter/map
     (for (x <- xs if x%2 == 0) yield x*10)    should be (xs.filter(_%2 == 0).map(_*10))
     // destructuring bind
     (for ((x,y) <- xs zip ys)  yield x*y)     should be ((xs zip ys) map { case (x,y) => x*y })
     // cross product
     (for (x <- xs; y <- ys)    yield x*y)     should be (xs flatMap {x => ys map {y => x*y}})
+  }
+
+  "singletons" should "be singletons" in {
+    val singleton = Singleton
   }
 
 }
